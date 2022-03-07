955a6318153f766b37ce4c05bae762cf4e4b09f4
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2022-03-07 09:37:36 +0700

    some change

--- app/src/main/java/com/example/mobile/view/RegisterActivity.java
+++ app/src/main/java/com/example/mobile/view/RegisterActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.mobile.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
