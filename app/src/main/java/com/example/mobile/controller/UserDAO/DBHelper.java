a9efe85a0c98798cf024e305945bbf305accf57a
peter_e <petersdm339@gmail.com>
2022-03-12 20:48:13 +0700

    seperate DAO and DBHelper, add searchable.xml, add Searchable Intent filter to MainActivity

--- app/src/main/java/com/example/mobile/controller/UserDAO/DBHelper.java
+++ app/src/main/java/com/example/mobile/controller/UserDAO/DBHelper.java
@@ -1,17 +1,7 @@
 package com.example.mobile.controller.UserDAO;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.telephony.PhoneNumberUtils;
-import android.util.Patterns;
-import android.widget.EditText;
-
-
-import com.example.mobile.model.User;
-import com.example.mobile.utils.Constant;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DBHelper extends SQLiteAssetHelper {
