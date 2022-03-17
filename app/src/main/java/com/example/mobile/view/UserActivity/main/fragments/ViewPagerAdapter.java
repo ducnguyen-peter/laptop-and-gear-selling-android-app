300a732a878ffbc1cfffa471ad73b9a2b7bc7e30
peter_e <petersdm339@gmail.com>
2022-03-17 10:46:27 +0700

    some adjustment for ViewPager for minimal Activity Stack

--- app/src/main/java/com/example/mobile/view/UserActivity/main/fragments/ViewPagerAdapter.java
+++ app/src/main/java/com/example/mobile/view/UserActivity/main/fragments/ViewPagerAdapter.java
@@ -7,13 +7,20 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.example.mobile.model.cart.Cart;
 import com.example.mobile.view.UserActivity.main.MainActivity;
 
 public class ViewPagerAdapter extends FragmentStateAdapter {
     private MainActivity mainActivity;
+    private HomeFragment homeFragment;
+    private CartFragment cartFragment;
+    private UserProfileFragment userProfileFragment;
     public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
         mainActivity = (MainActivity) fragmentActivity;
+        homeFragment = new HomeFragment(mainActivity);
+        cartFragment = new CartFragment();
+        userProfileFragment = new UserProfileFragment();
     }
 
     @NonNull
@@ -21,13 +28,13 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position){
             case 0:
-                return new HomeFragment(mainActivity);
+                return homeFragment;
             case 1:
-                return new CartFragment();
+                return cartFragment;
             case 2:
-                return new UserProfileFragment();
+                return userProfileFragment;
             default:
-                return new HomeFragment(mainActivity);
+                return homeFragment;
         }
     }
 
